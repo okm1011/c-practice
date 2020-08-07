bdcd72ec27db1c68d634bbe4c57d71eb6824c954
okm1011 <58724881+okm1011@users.noreply.github.com>
2020-08-08 06:07:36 +1200

    s

--- c practice1/c practice1/p 03.cpp
+++ c practice1/c practice1/p 03.cpp
@@ -1,16 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS     // scanf 보안 경고로 인한 컴파일 에러 방지
 #include <stdio.h>
+#include <stdlib.h>    // malloc, free 함수가 선언된 헤더 파일
+#include <string.h>    // strcpy 함수가 선언된 헤더 파일
+
+int main(void)
+{
+	int a, b;
+	scanf_s("%d %d", &a,&b);
+
+	int arr[3][4];
+	
+
+	
+		for (int j = 0; j < a; j++){
+			
+			for (int i = 0; i < b; i++)
+			{
+				scanf_s("%d", &arr[j][i]);
+
+			}
+		}
+
+		for (int j = 0; j < a; j++) {
+			int sum = 0;
+			for (int i = 0; i < b; i++)
+			{
+				sum += arr[j][i];
 
 
-int main() {
-	int arr[10] = { 1,3,5,7,9,11,13,15,17,18 };
+			}
+			
+			printf("%d\n",sum);
 
-	for (int i = 0; i <= 9; i++) {
-		int sum = arr[i] * 5;
+		}
+		
+		
 
-		printf("%d\n", sum);
+
 	}
 
-	}
+
+
+
+
+
+
+
 
 
 
